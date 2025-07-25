cb6cd5173039ecd6353ff2e316c2249181be5b30
Ban-gilhyeon <bbgiloo98@gmail.com>
2025-07-25 17:22:32 +0900

    baekjun1316

---
+++ problems/java/beakjoon/Baekjun1316_그룹단어체커.java
@@ -0,0 +1,33 @@
+package problems.java.beakjoon;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.Stack;
+
+public class Baekjun1316_그룹단어체커 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int tc = Integer.parseInt(br.readLine());
+        int answer = 0;
+        for(int i = 0; i < tc; i++){
+            Stack<String> check = new Stack<>();
+            String str = br.readLine();
+            char[] words = str.toCharArray();
+            String checkStr = "";
+            for(char c : words){
+                if(check.isEmpty() || check.peek().equals(String.valueOf(c)) || !checkStr.contains(String.valueOf(c))) {
+                    check.add(String.valueOf(c));
+                    checkStr += String.valueOf(c);
+                } else{
+                    checkStr += "0";
+                }
+            }
+            if(!checkStr.contains("0")){
+                answer++;
+            }
+        }
+        System.out.println(answer);
+    }
+}
