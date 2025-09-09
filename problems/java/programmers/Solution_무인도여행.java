8e7d079b22a8187238c4fdaa5f9c8a3b2f08ce9a
Ban-gilhyeon <bbgiloo98@gmail.com>
2025-09-09 19:00:10 +0900

    Initial commit

--- problems/java/programmers/Solution_무인도여행.java
+++ problems/java/programmers/Solution_무인도여행.java
@@ -1,4 +1,67 @@
 package problems.java.programmers;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+
 public class Solution_무인도여행 {
+    private static int dfs(int x, int y, List<List<Integer>> map, boolean[][] visited){
+        if(map.get(y).get(x) == -1) return 0;
+        if( y < 0 || y >= map.size() || x < 0 || x >= map.get(y).size()) return 0;
+        if(visited[y][x]) return 0;
+
+        int sum = map.get(y).get(x);
+        visited[y][x] = true;
+
+        int[] dy = {1, 0, 0, -1};
+        int[] dx = {0, 1, -1, 0};
+        for(int i = 0; i < 4; i++){
+            int ny = y + dy[i];
+            int nx = x + dx[i];
+            if(ny >= 0 && ny < map.size() && nx >= 0 && nx <map.get(ny).size()) sum += dfs( nx,ny, map, visited);
+        }
+        return sum;
+    }
+
+    public static void main(String[] args) {
+        List<Integer> answer = new ArrayList<>();
+        String[] strArr = {"X591X","X1X5X","X231X", "1XXX1"};
+        List<List<Integer>> map = new ArrayList<>();
+        boolean[][] visited = new boolean[strArr.length][];
+
+        for(int i = 0; i < strArr.length; i++){
+            map.add(new ArrayList<>());
+            char[] chars = strArr[i].toCharArray();
+            visited[i] = new boolean[chars.length];
+            Arrays.fill(visited[i], false);
+            for(char c : chars){
+                if(c == 'X'){
+                    map.get(i).add(-1);
+                }else{
+                    map.get(i).add(c - '0');
+                }
+            }
+        }
+        for(int i = 0; i < map.size(); i++){
+            for(int j = 0; j < map.get(i).size(); j++){
+                if(map.get(i).get(j) != -1 && !visited[i][j]){
+                    answer.add(dfs(j,i,map,visited));
+                }
+            }
+        }
+        if(answer.isEmpty()) answer.add(-1);
+        else Collections.sort(answer);
+
+        System.out.println(answer.size());
+        for(int i = 0; i <answer.size(); i++){
+            System.out.print(answer.get(i) + ", ");
+        }
+        for(int i = 0; i < map.size(); i++){
+            System.out.println("");
+            for(int j = 0; j < map.get(i).size(); j++){
+                System.out.print(map.get(i).get(j) + ", ");
+            }
+        }
+    }
 }
--- problems/java/programmers/Solution_삼각달팽이.java
+++ problems/java/programmers/Solution_삼각달팽이.java
@@ -1,4 +1,77 @@
 package problems.java.programmers;
 
+import java.util.Arrays;
+
 public class Solution_삼각달팽이 {
+    static private int cnt = 1;
+    private  static int[] solution(int n){
+        int [] answer = {};
+
+        int[][] map = new int[n][];
+        for(int i = n; i > 0; i--){
+            map[i] = new int[n];
+        }
+        for(int i = 0; i <= n; i++){
+            map[i][0]++;
+        }
+
+        for(int i = 0; i <= n; i++){
+            for(int j = 0; j < map[i].length; j++){
+                System.out.println(map[i][j]);
+            }
+        }
+        return answer;
+    }
+    private static void fillArray(int x, int y, int[][] map, boolean[][] visited, int limit, int n){
+        if(limit < cnt) return;
+        if(x < 0 || y < 0 || y >= n || x > map[y].length -1) return;
+        if(visited[y][x]) return;
+
+        map[y][x] = cnt++;
+        visited[y][x] = true;
+
+        int[] dy = {1,0,-1};
+        int[] dx = {0, 1, -1};
+
+    }
+    public static void main(String[] args) {
+        int n = 4;
+        int[][] map = new int[n][];
+        boolean[][] visited = new boolean[n][];
+        int cnt = 1;
+        int cal = 1;
+        int limit = 0;
+        for(int i = 0 ; i < n; i++){
+            map[i] = new int[cal];
+            Arrays.fill(map[i], 0);
+            limit += cal;
+            cal++;
+        }
+        int x = 0;
+        int y = 0;
+        int dir = 0;
+        while(limit >= cnt){
+            map[y][x] = cnt++;
+
+            int[] dy = {1, 0, -1};
+            int[] dx = {0, 1, -1};
+            int ny = y + dy[dir];
+            int nx = x + dx[dir];
+            if( nx < 0 || ny < 0 || ny >= n || nx >= map[y].length || map[ny][nx] != 0){
+                dir = (dir + 1) % 3;
+                ny = y + dy[dir];
+                nx = x + dx[dir];
+            }
+            x = nx;
+            y = ny;
+        }
+
+        for(int i = 0; i < n; i++){
+            System.out.println("");
+            for(int j = 0; j < map[i].length; j++){
+                System.out.print(map[i][j] +", ");
+            }
+        }
+
+    }
 }
